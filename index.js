ca248556c1f268c446be4fbf17b12b64271f38dd
rendnil <andrew.lindner.15@gmail.com>
2018-09-14 19:14:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+
+  function distanceFromHqInBlocks(value){
+    return Math.abs(value - 42)
+  }
+
+  function distanceFromHqInFeet(value){
+    return 264* distanceFromHqInBlocks(value)
+  }
+
+  function distanceTravelledInFeet(x, y){
+    return 264*Math.abs(x-y)
+  }
+
+
+  function calculatesFarePrice(x,y){
+    let ft = distanceTravelledInFeet(x,y)
+
+    if(ft<=400){
+      return 0
+    }
+
+    else if (ft > 400 && ft< 2000){
+      return (0.02*(ft-400))
+
+    }else if(ft>2000 && ft<=2500){
+      return 25
+    }else if (ft>2500){
+    return 'cannot travel that far'
+
+    }
+
+
+  }
