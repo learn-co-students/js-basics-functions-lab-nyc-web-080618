7508baf6d03f86fae8c1e13f5cd7c8aefa16a4f2
Yixuan Wang <ywang0506@gmail.com>
2018-09-16 12:01:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+ if (distance === 43){
+   return 1;
+ } else if (distance > 43){
+   return distance - 43 +1;
+ } else {
+   return 43 - distance -1;
+ }
+}
+
+function distanceFromHqInFeet(distance){
+  return distanceFromHqInBlocks(distance) * 264;
+}
+
+function distanceTravelledInFeet(d1, d2){
+  return  Math.abs(d2-d1) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+  if (distanceTravelledInFeet(start,destination)<= 400) {
+    return 0;
+  } else if (distanceTravelledInFeet(start,destination) > 2500){
+    return 'cannot travel that far';
+  } else if (distanceTravelledInFeet(start,destination) <= 2000) {
+    return (distanceTravelledInFeet(start,destination)-400) * 0.02
+  } else {
+    return 25;
+  }
+}
